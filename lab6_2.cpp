a3bc5a2c38aeadd412be633524f6c0680f1343aa
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2020-01-17 16:58:10 +0700

    ez lab  3 hours

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,35 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII 
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string name,movie,date,sixnine;
+    int yearcode;
+    
+cout << "Fahsai: Sawadee ka...Can you tell me your name? \n";
+cout << "?????: ";
+getline(cin,name);
+cout << "Wow!!! " << name << " that is a really cool name. \n";
+cout << "Fahsai: I think you are Engineering student. What is your student ID? \n";
+cout << name <<": ";
+cin >> yearcode;
+cout << "I think you may be #GEAR"<< (yearcode/10000000)-12 << ".  I have a free movie tickets for you. \n";
+cout << "Fahsai: Let's go to cinema together!!! \n";
+cout << "Fahsai: What movie do you want to watch? \n";
+cout << name <<": ";
+cin.ignore();
+getline(cin,movie);
+cout << "Fahsai: So....which day are you free to go with me? \n";
+cout << name <<": ";
+getline(cin,date);
+cout << date << "....that is OK!!! I'm looking forward to watch "<< movie << " with you.\n";
+cout << name <<": ";
+getline(cin,sixnine);
+cout << "Fahsai: 555+ see you "<< date <<". Bye Bye \\(^ ^)/ ";
+return 0;
+}
